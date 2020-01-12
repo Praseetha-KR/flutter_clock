b58d387cbedf300ed784d449bad6ff37118ba7ea
Praseetha-KR <praseetha04@gmail.com>
2020-01-12 17:16:37 +0530

    Layout with font fix

--- facts_clock/lib/facts_clock.dart
+++ facts_clock/lib/facts_clock.dart
@@ -14,18 +14,21 @@ import 'package:auto_size_text/auto_size_text.dart';
 enum _Element {
   background,
   text,
+  textAccent,
   accent,
 }
 
 final _lightTheme = {
-  _Element.background: Colors.white,
+  _Element.background: Color(0xFFF4F8FB),
   _Element.text: Colors.black,
+  _Element.textAccent: Colors.black54,
   _Element.accent: Colors.deepPurple,
 };
 
 final _darkTheme = {
-  _Element.background: Color(0xFF1D1E1E),
+  _Element.background: Color(0xFF13192D),
   _Element.text: Colors.white,
+  _Element.textAccent: Colors.white70,
   _Element.accent: Colors.deepPurple,
 };
 
@@ -41,9 +44,7 @@ Future loadFactsData() async {
     return jsonResponse;
 }
 
-/// A basic digital clock.
-///
-/// You can do better than this!
+
 class FactsClock extends StatefulWidget {
   const FactsClock(this.model);
 
@@ -109,12 +110,12 @@ class _FactsClockState extends State<FactsClock> {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).brightness == Brightness.light
+          ? _lightTheme
+          : _darkTheme;
     // final colors = Theme.of(context).brightness == Brightness.light
-    //     ? _lightTheme
-    //     : _darkTheme;
-      final colors = Theme.of(context).brightness == Brightness.light
-        ? _lightTheme
-        : _darkTheme;
+    //       ? _darkTheme
+    //       : _lightTheme;
     final hour = DateFormat(
       widget.model.is24HourFormat ? 'HH' : 'hh'
     ).format(_dateTime);
@@ -133,15 +134,23 @@ class _FactsClockState extends State<FactsClock> {
         fact = "Good Night!";
     }
     if (factsData[factKey]!= null && factsData[factKey].length > 0){
-        fact = factsData[factKey][0];
+        fact = "💡 " + factsData[factKey][0];
     }
 
     final fontSize = MediaQuery.of(context).size.width / 20.5;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     final offset = -fontSize / 7;
     final defaultStyle = TextStyle(
       color: colors[_Element.text],
       fontFamily: 'Roboto',
       fontSize: fontSize,
+    );
+
+    final timeStyle = TextStyle(
+      color: colors[_Element.text],
+      fontFamily: 'Fjalla One',
+      // fontWeight: FontWeight.w600,
     );
 
     return Container(
@@ -161,7 +170,7 @@ class _FactsClockState extends State<FactsClock> {
             ),
           ),
           Flexible(
-            flex: 48,
+            flex: 64,
             child: Padding(
               padding: EdgeInsets.fromLTRB(32.0, 20.0, 32.0, 0.0),
               child: Container(
@@ -179,36 +188,47 @@ class _FactsClockState extends State<FactsClock> {
                             minFontSize: 0,
                             textAlign: TextAlign.end,
                             stepGranularity: 0.1,
-                            style: TextStyle(
-                                // fontFamily: 'Rajdhani',
-                                fontWeight: FontWeight.w600,
-                            ),
+                            style: timeStyle,
                           )
                         ),
-                        AutoSizeText.rich(
-                          TextSpan(
-                            text: ':',
-                            style: TextStyle(fontSize: 170, color: colors[_Element.accent]),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            left: screenWidth * 0.004,
+                            right: screenWidth * 0.004
                           ),
-                          minFontSize: 0,
-                          stepGranularity: 0.1,
-                          style: TextStyle(
-                            //   fontFamily: 'Rajdhani',
-                              fontWeight: FontWeight.w600,
+                          child: AutoSizeText.rich(
+                            TextSpan(
+                              text: ':',
+                              style: TextStyle(fontSize: 165, color: colors[_Element.accent]),
                             ),
+                            minFontSize: 0,
+                            stepGranularity: 0.1,
+                            style: timeStyle,
+                          ),
                         ),
                         Expanded( // Constrains AutoSizeText to the width of the Row
                           child: AutoSizeText.rich(
                             TextSpan(
                               text: minute,
-                              style: TextStyle(fontSize: 190),
+                              style: TextStyle(
+                                fontSize: 190,
+                                // shadows: [
+                                //   Shadow(
+                                //     blurRadius: 10.0,
+                                //     color: Colors.blue,
+                                //     offset: Offset(5.0, 5.0),
+                                //   ),
+                                //   Shadow(
+                                //     color: Colors.green,
+                                //     blurRadius: 10.0,
+                                //     offset: Offset(-10.0, 5.0),
+                                //   ),
+                                // ],
+                              ),
                             ),
                             minFontSize: 0,
                             stepGranularity: 0.1,
-                            style: TextStyle(
-                                // fontFamily: 'Rajdhani',
-                                fontWeight: FontWeight.w600,
-                            ),
+                            style: timeStyle,
                           )
                         ),
                       ],
@@ -218,134 +238,59 @@ class _FactsClockState extends State<FactsClock> {
               )
             ),
           ),
+          // Flexible(
+          //   flex: 10,
+          //   child: AutoSizeText.rich(
+          //     TextSpan(
+          //       text: 'Did you know?',
+          //       style: TextStyle(
+          //         // fontFamily: 'Open Sans',
+          //         fontSize: 15,
+          //         color: colors[_Element.accent],
+          //       ),
+          //     ),
+          //     minFontSize: 0,
+          //     textAlign: TextAlign.center,
+          //     stepGranularity: 0.1,
+          //   ),
+          // ),
           Flexible(
             flex: 25,
             child: Padding(
-              padding: EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 38.0),
+              padding: EdgeInsets.fromLTRB(
+                screenWidth * 0.08,
+                screenHeight * 0.04,
+                screenWidth * 0.08,
+                screenHeight * 0.08
+              ),
               child: Container(
-                child: Row(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Expanded( // Constrains AutoSizeText to the width of the Row
+                    Expanded(
                       child: AutoSizeText.rich(
                         TextSpan(
                           text: fact,
                           style: TextStyle(
-                              fontSize: 200,
-                            //   fontFamily: 'Open Sans',
-                            ),
+                            fontFamily: 'Open Sans',
+                            fontSize: 180,
+                            color: colors[_Element.textAccent],
+                          ),
                         ),
                         minFontSize: 0,
                         textAlign: TextAlign.center,
                         stepGranularity: 0.1,
-                      )
-                    ),
+                      ),
+                    )
                   ],
-                ),
+                )
               ),
-            ),
+            )
           ),
         ]
       ),
     );
   }
-
-
-  //   return Container(
-  //     color: colors[_Element.background],
-  //     width: MediaQuery.of(context).size.width,
-  //     child: Column(
-  //       crossAxisAlignment: CrossAxisAlignment.start,
-  //       children: <Widget>[
-  //         Container(
-  //           width: MediaQuery.of(context).size.width / 60 * int.parse(second),
-  //           height: 10.0,
-  //           color: Colors.deepPurple,
-  //         ),
-  //         Container(
-  //           child: FractionallySizedBox(
-  //             // alignment: Alignment.center,
-  //             widthFactor: 1,
-  //             heightFactor: 0.7,
-  //             child: Text(
-  //               hour,
-  //               style: TextStyle(color: Colors.white),
-  //             ),
-  //             // child: Row(
-  //             //   crossAxisAlignment: CrossAxisAlignment.center,
-  //             //   children: <Widget>[
-  //             //     Text(
-  //             //       hour,
-  //             //       style: TextStyle(color: Colors.white),
-  //             //     ),
-  //             //     Text(
-  //             //       ":",
-  //             //       style: TextStyle(color: Colors.deepPurple),
-  //             //     ),
-  //             //     Text(
-  //             //       minute,
-  //             //       style: TextStyle(color: Colors.white),
-  //             //     ),
-  //             //   ],
-  //             // )
-  //           ),
-  //         ),
-
-
-  //         // Center(
-  //         //   child: DefaultTextStyle(
-  //         //     style: defaultStyle,
-  //         //     child: new Container(
-  //         //       width: MediaQuery.of(context).size.width,
-  //         //       color: Colors.red,
-  //         //       margin: const EdgeInsets.all(0.0),
-  //         //       child: new Column(
-  //         //         mainAxisAlignment: MainAxisAlignment.center,
-  //         //         children: <Widget>[
-  //         //           new Align(
-  //         //             alignment: Alignment.bottomCenter,
-  //         //             child: new ButtonBar(
-  //         //               alignment: MainAxisAlignment.center,
-  //         //               children: <Widget>[
-  //         //                 new Text(
-  //         //                   hour,
-  //         //                   style: new TextStyle(color: Colors.white),
-  //         //                 ),
-  //         //                 new Text(
-  //         //                   minute,
-  //         //                   style: new TextStyle(color: Colors.white),
-  //         //                 ),
-  //         //                 // new Text(
-  //         //                 //   second,
-  //         //                 //   style: new TextStyle(color: Colors.white),
-  //         //                 // )
-  //         //               ],
-  //         //             ),
-  //         //           ),
-  //         //         ],
-  //         //       ),
-  //         //       //   Positioned(left: offset, top: 0, child: Text(hour)),
-  //         //       //   Positioned(right: offset, top: 0, child: Text(minute)),
-  //         //       //   Positioned(left: offset, top: -10 * offset, child: Text(second)),
-  //         //       //   Positioned(left: offset, bottom: 0, child: Text(fact)),
-  //         //       // ],
-  //         //     ),
-  //         //   ),
-  //         // ),
-  //         Center(
-  //           child: Text(
-  //             fact,
-  //             style: defaultStyle,
-  //             textAlign: TextAlign.center,
-  //           ),
-  //         ),
-  //         // Stack(
-  //         //   children: <Widget>[
-  //         //     Positioned(bottom: 0, child: Text(fact)),
-  //         //   ],
-  //         // )
-  //       ],
-  //     )
-  //   );
-  // }
 }
 
